89e0c459668ff7d41921583e93dfffa865233400
Esteban Ramirez Hincapie <ceramirez@posteo.net>
2022-12-07 16:02:30 +0100

    Memory game added and working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import ScrambleLogic from "./Components/ScrambleLogic";
 
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
+import MemoryApp from "./Pages/MemoryPage";
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<ScrambleLogic />} />
+        <Route path="/memory" element={<MemoryApp />} />
+      </Routes>
+
       {/* <ScramblePage /> */}
-      <ScrambleLogic />
+      {/* <ScrambleLogic /> */}
     </div>
   );
 }
---
+++ src/Pages/MemoryPage.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import MemoryGame from "../Components/MemoryGame";
+import "./GameApp.css";
+
+const MemoryApp = () => {
+  const [options, setOptions] = useState(null);
+  const [highScore, setHighScore] = useState(0);
+
+  useEffect(() => {
+    const json = localStorage.getItem("memorygamehighscore");
+    const savedScore = JSON.parse(json);
+    if (savedScore) {
+      setHighScore(savedScore);
+    }
+  }, []);
+
+  return (
+    <div>
+      <div className="container">
+        <h1>Memory Game</h1>
+        <div>High Score: {highScore}</div>
+        <div>
+          {options === null ? (
+            <>
+              <button onClick={() => setOptions(12)}>Easy</button>
+              <button onClick={() => setOptions(18)}>Medium</button>
+              <button onClick={() => setOptions(24)}>Hard</button>
+            </>
+          ) : (
+            <>
+              <button
+                onClick={() => {
+                  const prevOptions = options;
+                  setOptions(null);
+                  setTimeout(() => {
+                    setOptions(prevOptions);
+                  }, 5);
+                }}
+              >
+                Start Over
+              </button>
+              <button onClick={() => setOptions(null)}>Main Menu</button>
+            </>
+          )}
+        </div>
+      </div>
+
+      {options ? (
+        <MemoryGame
+          options={options}
+          setOptions={setOptions}
+          highScore={highScore}
+          setHighScore={setHighScore}
+        />
+      ) : (
+        <h2>Choose a difficulty to begin!</h2>
+      )}
+    </div>
+  );
+};
+
+export default MemoryApp;
